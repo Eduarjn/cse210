901e1d3586f274e6870fc627e1fb1d042ab5279a
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-08-05 13:59:03 -0300

    Implement shape management in Program.cs

---
+++ w06/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace W06
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Shape> shapes = new List<Shape>();
+
+      Square s1 = new Square("Red", 3);
+      shapes.Add(s1);
+
+      Rectangle r1 = new Rectangle("Blue", 4, 5);
+      shapes.Add(r1);
+
+      Circle c1 = new Circle("Green", 6);
+      shapes.Add(c1);
+
+      foreach (Shape s in shapes)
+      {
+        string color = s.GetColor();
+        double area = s.GetArea();
+        Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+      }
+    }
+  }
+}
