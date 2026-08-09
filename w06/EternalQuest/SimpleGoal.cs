2291ab0b0b15e165d2eb6d93e84f7143c54e6339
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-08-09 17:39:37 -0300

    Refactor SimpleGoal methods for consistency

--- w06/EternalQuest/SimpleGoal.cs
+++ w06/EternalQuest/SimpleGoal.cs
@@ -2,30 +2,30 @@ using System;
 
 public class SimpleGoal : Goal
 {
-private bool _isComplete;
+  private bool _isComplete;
 
-public SimpleGoal(string name, string description, int points) : base(name, description, points)
-{
-_isComplete = false;
-}
+  public SimpleGoal(string name, string description, int points) : base(name, description, points)
+  {
+    _isComplete = false;
+  }
 
-public override int RecordEvent()
-{
-if (!_isComplete)
-{
-_isComplete = true;
-return _points;
-}
-return 0;
-}
+  public override int RecordEvent()
+  {
+    if (!_isComplete)
+    {
+      _isComplete = true;
+      return GetPoints();
+    }
+    return 0;
+  }
 
-public override bool IsComplete()
-{
-return _isComplete;
-}
+  public override bool IsComplete()
+  {
+    return _isComplete;
+  }
 
-public override string GetStringRepresentation()
-{
-return $"SimpleGoal:{_shortName},{_description},{_points},{_isComplete}";
-}
+  public override string GetStringRepresentation()
+  {
+    return $"SimpleGoal:{GetShortName()},{GetDescription()},{GetPoints()},{_isComplete}";
+  }
 }
