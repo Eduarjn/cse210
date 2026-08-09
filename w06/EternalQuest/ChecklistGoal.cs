1b7fbc22f8f89fe45bc90fde783782627cfbd655
Eduarjn <eduarjose.fajardo96@gmail.com>
2026-08-09 17:41:36 -0300

    Refactor ChecklistGoal methods for consistency

--- w06/EternalQuest/ChecklistGoal.cs
+++ w06/EternalQuest/ChecklistGoal.cs
@@ -2,44 +2,44 @@ using System;
 
 public class ChecklistGoal : Goal
 {
-private int _amountCompleted;
-private int _target;
-private int _bonus;
+  private int _amountCompleted;
+  private int _target;
+  private int _bonus;
 
-public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
-{
-_amountCompleted = 0;
-_target = target;
-_bonus = bonus;
-}
+  public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+  {
+    _amountCompleted = 0;
+    _target = target;
+    _bonus = bonus;
+  }
 
-public override int RecordEvent()
-{
-if (!IsComplete())
-{
-_amountCompleted++;
-if (_amountCompleted == _target)
-{
-return _points + _bonus;
-}
-return _points;
-}
-return 0;
-}
+  public override int RecordEvent()
+  {
+    if (!IsComplete())
+    {
+      _amountCompleted++;
+      if (_amountCompleted == _target)
+      {
+        return GetPoints() + _bonus;
+      }
+      return GetPoints();
+    }
+    return 0;
+  }
 
-public override bool IsComplete()
-{
-return _amountCompleted >= _target;
-}
+  public override bool IsComplete()
+  {
+    return _amountCompleted >= _target;
+  }
 
-public override string GetDetailsString()
-{
-string status = IsComplete() ? "[X]" : "[ ]";
-return $"{status} {_shortName} ({_description}) -- Currently completed: {_amountCompleted}/{_target}";
-}
+  public override string GetDetailsString()
+  {
+    string status = IsComplete() ? "[X]" : "[ ]";
+    return $"{status} {GetShortName()} ({GetDescription()}) -- Currently completed: {_amountCompleted}/{_target}";
+  }
 
-public override string GetStringRepresentation()
-{
-return $"ChecklistGoal:{_shortName},{_description},{_points},{_bonus},{_target},{_amountCompleted}";
-}
+  public override string GetStringRepresentation()
+  {
+    return $"ChecklistGoal:{GetShortName()},{GetDescription()},{GetPoints()},{_bonus},{_target},{_amountCompleted}";
+  }
 }
